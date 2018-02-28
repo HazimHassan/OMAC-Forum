d6f45da837dce140f51b8527b66f2d27dc05ca3a
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-01 00:43:07 +0200

    Update ATM withdraw function

--- ATM-Solution/atm.py
+++ ATM-Solution/atm.py
@@ -1,26 +1,28 @@
-def withdraw(R, C):
-    if R > C:
-        print "$500 max"
-    elif R <= 0:
+def withdraw(request, balance):
+    if request > balance:
+        print "$"+str(balance)+" max"
+    elif request <= 0:
         print "Invalid request"
     else:
-        while R > 0:
-            if R >= 100:
-                R = R - 100
+        while request > 0:
+            if request >= 100:
+                request -= 100
                 print "give $100"
-            elif R >= 50:
-                R = R - 50
+            elif request >= 50:
+                request -= 50
                 print"give $50"
-            elif R >= 20:
-                R = R - 20
+            elif request >= 20:
+                request -= 20
                 print"give $20"
-            elif R >= 10:
-                R = R - 10
+            elif request >= 10:
+                request -= 10
                 print"give $10"
-            elif R >= 5:
-                R = R - 5
+            elif request >= 5:
+                request -= 5
                 print"give $5"
             else:
-                R = R - 1
+                request -= 1
                 print"give $1"
-print withdraw(100, 500)
+#withdraw(120, 500)
+#withdraw(297, 380)
+withdraw(90, 83)
