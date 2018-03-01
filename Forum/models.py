8598f8313c1b050c98c07a5d0e3f89736be9fc49
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-01 11:58:57 +0200

    Add get by id, entity exists and delete functions to stores

--- Forum/models.py
+++ Forum/models.py
@@ -1,11 +1,14 @@
 class Member():
 
     def __init__(self, name, age):
+        self.id = 0
         self.name = name
         self.age = age
 
+
 class Post():
 
-    def __init__(self,title, body):
+    def __init__(self, title, body):
+        self.id = 0
         self.title = title
         self.body = body
--- Forum/stores.py
+++ Forum/stores.py
@@ -1,29 +1,55 @@
 class MemberStore:
     members = []
+    last_id = 1
 
     def add(self, member):
-        self.members.append(member)
+        member.id = MemberStore.last_id
+        MemberStore.members.append(member)
+        MemberStore.last_id += 1
 
     def get_all(self):
-        return self.members
-'''
-    def get_by_id(self, id):
-      # search for member by id
+        return MemberStore.members
 
-    def update(self, member):
-     # update member data
+    def get_by_id(self, id):
+        for member in MemberStore.members:
+            if id == member.id:
+                return member
+        return "Not found"
 
     def delete(self, id):
-      # delete member by id
+        member = self.get_by_id(id)
+        MemberStore.members.remove(member)
 
     def entity_exists(self, member):
-      # checks if an entity exists in a store
-'''
+        #result = "Exists"
+        if self.get_by_id(member.id) == "Not found":
+            return  "Not Exists"
+        return "Exists"
+
 class PostStore:
     posts = []
+    last_id = 1
 
     def add(self, post):
-        self.posts.append(post)
+        post.id = PostStore.last_id
+        PostStore.posts.append(post)
+        PostStore.last_id += 1
 
     def get_all(self):
         return self.posts
+
+    def get_by_id(self, id):
+        for post in PostStore.posts:
+            if id == post.id:
+                return post
+        return "Not found"
+
+    def delete(self, id):
+        post = self.get_by_id(id)
+        PostStore.posts.remove(post)
+
+    def entity_exists(self, post):
+        #result = "Exists"
+        if self.get_by_id(post.id) == "Not found":
+            return  "Not Exists"
+        return "Exists"
