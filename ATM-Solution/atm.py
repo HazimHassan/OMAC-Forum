03790c0f1200cf8cec56d6456e50ee5378670a4c
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-01 05:59:46 +0200

    Add show withdrawals function

--- ATM-Solution/atm.py
+++ ATM-Solution/atm.py
@@ -1,6 +1,7 @@
 class ATM():
 
     def __init__(self, balance, bank_name):
+        self.withdrawals_list = []
         self.balance = balance
         self.bank_name = bank_name
 
@@ -17,7 +18,8 @@ class ATM():
             return self.balance
         else:
             print "Welcome to " +(self.bank_name)
-            (self.balance) -= request
+            self.withdrawals_list.append(request)
+            self.balance -= request
             while request > 0:
                 if request >= 100:
                     request -= 100
@@ -41,6 +43,10 @@ class ATM():
             print "************************** "
             return self.balance
 
+    def show_withdrawals(self):
+        for withdrawal in self.withdrawals_list:
+            print withdrawal
+
 BALANCE1 = 500
 ATM1 = ATM(BALANCE1, "Smart Bank")
 
@@ -50,5 +56,12 @@ ATM2 = ATM(BALANCE2, "Baraka Bank")
 ATM1.withdraw(277)
 ATM1.withdraw(800)
 
+
 ATM2.withdraw(100)
 ATM2.withdraw(2000)
+
+
+print "ATM 1 withdrawals:"
+ATM1.show_withdrawals()
+print "ATM 2 withdrawals:"
+ATM2.show_withdrawals()
