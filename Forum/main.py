453484524a8ab909885c8d2f693004385ac70bb6
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-01 07:10:41 +0200

    Add Member and Post classes and entities

---
+++ Forum/main.py
@@ -0,0 +1,8 @@
+import models
+
+Member1 = models.Member("Member1", "26")
+Member2 = models.Member("Member2", "30")
+
+Post1 = models.Post("title1","body1")
+Post2 = models.Post("title2","body2")
+Post3 = models.Post("title3","body3")
