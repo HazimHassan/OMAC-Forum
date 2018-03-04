f185bf21096d03941c32bef2bba7a5f6f6bd35ad
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-04 20:50:14 +0200

    Add Process_request function

--- ATM-Solution/atm.py
+++ ATM-Solution/atm.py
@@ -6,42 +6,42 @@ class ATM():
         self.bank_name = bank_name
 
     def withdraw(self, request):
+        print "Welcome to " +(self.bank_name)
+        print "Current balance = " + str(self.balance)
         if request > self.balance:
-            print "Welcome to " +(self.bank_name)
             print "$"+str(self.balance)+" max"
             print "************************** "
-            return self.balance
         elif request <= 0:
-            print "Welcome to " +(self.bank_name)
             print "Invalid request"
-            print "************************** "
-            return self.balance
+            print "************************** "        
         else:
-            print "Welcome to " +(self.bank_name)
             self.withdrawals_list.append(request)
             self.balance -= request
-            while request > 0:
-                if request >= 100:
-                    request -= 100
-                    print "give $100"
-                elif request >= 50:
-                    request -= 50
-                    print"give $50"
-                elif request >= 20:
-                    request -= 20
-                    print"give $20"
-                elif request >= 10:
-                    request -= 10
-                    print"give $10"
-                elif request >= 5:
-                    request -= 5
-                    print"give $5"
-                else:
-                    request -= 1
-                    print"give $1"
-            print "Current Balance " + "$" + str(self.balance)
-            print "************************** "
-            return self.balance
+            ATM.process_request(self, request)
+
+    def process_request(self, request):
+
+        while request > 0:
+            if request >= 100:
+                request -= 100
+                print "give $100"
+            elif request >= 50:
+                request -= 50
+                print"give $50"
+            elif request >= 20:
+                request -= 20
+                print"give $20"
+            elif request >= 10:
+                request -= 10
+                print"give $10"
+            elif request >= 5:
+                request -= 5
+                print"give $5"
+            else:
+                request -= 1
+                print"give $1"
+        print "************************** "
+        return self.balance
 
     def show_withdrawals(self):
         for withdrawal in self.withdrawals_list:
@@ -56,10 +56,8 @@ ATM2 = ATM(BALANCE2, "Baraka Bank")
 ATM1.withdraw(277)
 ATM1.withdraw(800)
 
-
 ATM2.withdraw(100)
 ATM2.withdraw(2000)
-
 
 print "ATM 1 withdrawals:"
 ATM1.show_withdrawals()
