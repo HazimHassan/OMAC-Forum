fd6a4d4a7687e9ff39f9ebf651b491b75ecc8a8e
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-16 22:47:46 +0200

    Add get members with posts and get top tow functions

--- Forum/stores.py
+++ Forum/stores.py
@@ -1,3 +1,6 @@
+import copy
+import itertools
+
 class MemberStore():
     members = []
     last_id = 1
@@ -19,15 +22,23 @@ class MemberStore():
                 break
         return result
 
-    def get_by_name(self, id):
-        all_members = self.get_all()
-        result = None
-        for member in all_members:
-            if member.name == name:
-                result = member
-                break
-        return result
+    def get_by_name(self, member_name):
+        return (member for member in self.get_all() if member.name == member_name)
 
+    def get_members_with_posts(self, all_posts):
+        all_members = copy.deepcopy(self.get_all())
+        all_product = itertools.product(all_members, posts)
+        for member, post in all_product:
+            if member.id == post.member_id:
+                member.posts.append(post)
+        return (member for member in all_members)
+    
+    def get_top_tow(self, all_posts):
+        member_with_posts = list(self.get_members_with_posts(all_posts))
+        member_with_posts.sort(key=lambda member: len(member.posts), reverse=True)
+        yield member_with_posts[0]
+        yield member_with_posts[1]
+    
     def delete(self, id):
         member = self.get_by_id(id)
         MemberStore.members.remove(member)
