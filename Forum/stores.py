6510f9743eec1852ee978b0071b13e22af6502e9
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-16 21:25:59 +0200

    Add get by name and update functions

--- Forum/stores.py
+++ Forum/stores.py
@@ -11,10 +11,22 @@ class MemberStore():
         return MemberStore.members
 
     def get_by_id(self, id):
-        for member in MemberStore.members:
-            if id == member.id:
-                return member
-        return None
+        all_members = self.get_all()
+        result = None
+        for member in all_members:
+            if member.id == id:
+                result = member
+                break
+        return result
+
+    def get_by_name(self, id):
+        all_members = self.get_all()
+        result = None
+        for member in all_members:
+            if member.name == name:
+                result = member
+                break
+        return result
 
     def delete(self, id):
         member = self.get_by_id(id)
@@ -26,6 +38,12 @@ class MemberStore():
         return True
     
     def update(self, member):
+        result = member
+        all_members = self.get_all()
+        for index, current_member in enumerate(all_members):
+            if current_member.id == member.id:
+                all_members[index] = member
+        return result
 
 class PostStore():
     posts = []
