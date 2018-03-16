a12d9976a2e291bc90729b1405e87435437e4bed
Hazem Elsamani Hassan <smany.hh@gmail.com>
2018-03-16 23:35:24 +0200

    Add BaseStore class

--- Forum/stores.py
+++ Forum/stores.py
@@ -1,89 +1,81 @@
-import copy
 import itertools
-
-class MemberStore():
-    members = []
-    last_id = 1
+import copy
+ 
+class BaseStore():
+ 
+    def __init__(self, data_provider, last_id):
+        self._data_provider = data_provider
+        self._last_id = last_id
+ 
+    def get_all(self):
+        return self._data_provider
 
     def add(self, member):
-        member.id = MemberStore.last_id
-        MemberStore.members.append(member)
-        MemberStore.last_id += 1
-
-    def get_all(self):
-        return MemberStore.members
-
+        member.id = self._last_id
+        self._data_provider.append(member)
+        self._last_id += 1
+ 
     def get_by_id(self, id):
-        all_members = self.get_all()
         result = None
-        for member in all_members:
-            if member.id == id:
-                result = member
+        all_model_instances = self.get_all()
+        for e in all_model_instances:
+            if e.id == id:
+                result = e
                 break
         return result
 
-    def get_by_name(self, member_name):
-        return (member for member in self.get_all() if member.name == member_name)
-
-    def get_members_with_posts(self, all_posts):
-        all_members = copy.deepcopy(self.get_all())
-        all_product = itertools.product(all_members, posts)
-        for member, post in all_product:
-            if member.id == post.member_id:
-                member.posts.append(post)
-        return (member for member in all_members)
-    
-    def get_top_tow(self, all_posts):
-        member_with_posts = list(self.get_members_with_posts(all_posts))
-        member_with_posts.sort(key=lambda member: len(member.posts), reverse=True)
-        yield member_with_posts[0]
-        yield member_with_posts[1]
-    
-    def delete(self, id):
-        member = self.get_by_id(id)
-        MemberStore.members.remove(member)
-
-    def entity_exists(self, member):
-        if self.get_by_id(member.id) == None:
-            return  False
-        return True
-    
-    def update(self, member):
-        result = member
-        all_members = self.get_all()
-        for index, current_member in enumerate(all_members):
-            if current_member.id == member.id:
-                all_members[index] = member
+    def entity_exists(self, model_instance):
+        result = False
+        if self.get_by_id(model_instance.id) is not None:
+            result = True
         return result
 
-class PostStore():
-    posts = []
+    def delete(self, id):
+        model_instance = self.get_by_id(id)
+        all_model_instances = self.get_all()
+        all_model_instances.remove(model_instance)
+
+    def update(self, model_instance):
+        all_model_instances = self.get_all()
+        for i, p in enumerate(all_model_instances):
+            if model_instance.id == p.id:
+                all_model_instances[i] = model_instance
+                break
+ 
+class MemberStore(BaseStore):
+    members = []
     last_id = 1
 
-    def add(self, post):
-        post.id = PostStore.last_id
-        PostStore.posts.append(post)
-        PostStore.last_id += 1
-
-    def get_all(self):
-        return self.posts
-
-    def get_by_id(self, id):
-        for post in PostStore.posts:
-            if id == post.id:
-                return post
-        return None
+    def __init__(self):
+        super().__init__(MemberStore.members, MemberStore.last_id)
+ 
+    def get_by_name(self, name):
+        all_members = self.get_all()
+        for member in all_members:
+            if member.name == name:
+                yield member
+ 
+    def get_members_with_posts(self, all_posts):
+        all_members = copy.deepcopy(self.get_all())
+        for member, post in itertools.product(all_members, all_posts):
+            if member.id == post.member_id:
+                member.posts.append(post)
+        for member in all_members:
+            yield member
+ 
+    def get_top_two(self, post_store):
+        all_members = self.get_members_with_posts(post_store)
+        all_members = sorted(all_members, key=lambda x: len(x.posts), reverse=True)
+        return all_members[:2]
+ 
+class PostStore(BaseStore):
+    posts = []
+    last_id = 1
+ 
+    def __init__(self):
+        super().__init__(PostStore.posts, PostStore.last_id)
     
     def get_posts_by_date(self):
         all_posts = self.get_all()
-        all_posts.sort(key=lambda post: post.date, reverse=True) 
+        all_posts.sort(key=lambda post: post.date, reverse=True)
         return (post for post in all_posts)
-
-    def delete(self, id):
-        post = self.get_by_id(id)
-        PostStore.posts.remove(post)
-
-    def entity_exists(self, post):
-        if self.get_by_id(post.id) == None:
-            return  False
-        return True
